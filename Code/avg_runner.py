89ddc9289b0a7d09d298afc524b12262d0d53916
haleqiu <115010216@link.cuhk.edu.cn>
2018-09-08 21:24:08 +0800

    yuhan

--- Code/avg_runner.py
+++ Code/avg_runner.py
@@ -55,7 +55,7 @@ class AVGRunner:
 
         # if load path specified, load a saved model
         if model_load_path is not None:
-            self.saver.restore(self.sess, model_load_path)
+            self.saver.restore(self.sess, tf.train.latest_checkpoint(model_load_path))
             print 'Model restored from ' + model_load_path
 
     def train(self):
